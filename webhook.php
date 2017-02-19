04c94ecc7acb0af8e1686e82587c8b55a9cf954b
Rachit Goel <rachit2goel@gmail.com>
2017-02-19 22:27:37 +0530

    Database update through webhook

--- webhook.php
+++ webhook.php
@@ -1,6 +1,12 @@
 <?php
 require '../include.php';
 
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
 $data = $_POST;
 $postdata = json_encode($_POST);
 $mac_provided = $data['mac'];  // Get the MAC from the POST data
@@ -37,7 +43,43 @@ if($mac_provided == $mac_calculated){
 
         $json = json_decode($response, true);        
 
+        $statusr = $json['payment_request']['status'];
+        $purpose = $json['payment_request']['purpose'];
+        $payment_id = $json['payment_request']['payment']['payment_id'];
+        $quantity = $json['payment_request']['payment']['quantity'];
+        $status = $json['payment_request']['payment']['status'];
+        $link_slug = $json['payment_request']['payment']['link_slug'];
+        $link_title = $json['payment_request']['payment']['link_title'];
+        $buyer_name = $json['payment_request']['payment']['buyer_name'];
+        $buyer_phone = $json['payment_request']['payment']['buyer_phone'];
         $buyer_email = $json['payment_request']['payment']['buyer_email'];
+        $currency = $json['payment_request']['payment']['currency'];
+        $unit_price = $json['payment_request']['payment']['unit_price'];
+        $amount = $json['payment_request']['payment']['amount'];
+        $fees = $json['payment_request']['payment']['fees'];
+        $shipping_address = $json['payment_request']['payment']['shipping_address'];
+        $shipping_city = $json['payment_request']['payment']['shipping_city'];
+        $shipping_state = $json['payment_request']['payment']['shipping_state'];
+        $shipping_zip = $json['payment_request']['payment']['shipping_zip'];
+        $shipping_country = $json['payment_request']['payment']['shipping_country'];
+        $discount_code = $json['payment_request']['payment']['discount_code'];
+        $discount_amount_off = $json['payment_request']['payment']['discount_amount_off'];
+        $variants = $json['payment_request']['payment']['variants'];
+        $custom_fields = $json['payment_request']['payment']['custom_fields'];
+        $affiliate_id = $json['payment_request']['payment']['affiliate_id'];
+        $affiliate_commission = $json['payment_request']['payment']['affiliate_commission'];
+        $created_at = $json['payment_request']['payment']['created_at'];
+        date_default_timezone_set("Asia/Calcutta");
+        $dateIndia = date("Y-m-d "."h:i:sa");
+        $sql = "INSERT INTO payment ( payment_id, quantity, status, link_slug, link_title, buyer_name, buyer_phone, buyer_email, currency, unit_price, amount, fees, shipping_address, shipping_city, shipping_state, shipping_zip, shipping_country, discount_code, discount_amount_off, affiliate_id, affiliate_commission, created_at) VALUES ( '$payment_id', '$quantity', '$status', '$link_slug', '$link_title', '$buyer_name', '$buyer_phone', '$buyer_email', '$currency', '$unit_price', '$amount', '$fees', '$shipping_address', '$shipping_city', '$shipping_state', '$shipping_zip', '$shipping_country', '$discount_code', '$discount_amount_off', '$affiliate_id', '$affiliate_commission', '$created_at' )";
+
+
+        $sql = "UPDATE request SET status = '$statusr' WHERE id = '$_GET[payment_request_id]'";
+
+
+        mysqli_close($conn);
+
+
 
         date_default_timezone_set("Asia/Calcutta");
         $dateIndia = date("Y-m-d "."h:i:sa");
@@ -46,7 +88,7 @@ if($mac_provided == $mac_calculated){
 
 
         $to = $buyer_email;
-        $subject = 'Thank You For your Payment';
+        $subject = "Thank You For your Payment";
               
         $message = "Thank You For your Payment";
                  
